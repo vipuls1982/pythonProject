3ec4bff793ed29e57c352fa84db4458f4870c6aa
vipuls1982 <137588592+vipuls1982@users.noreply.github.com>
2023-07-01 07:34:20 -0700

    remove main import

--- substraction.py
+++ substraction.py
@@ -3,29 +3,35 @@ import time
 import os
 
 os.system('cls')
-count=int(input("how many addition you want to practice:"))
+count=int(input("how many substraction you want to practice:"))
 
 
 for i in range(1, count+1):
 
     os.system('cls')
     a=randint(10,99)
-    b=randint(10,99)
-    tot=a+b
-    once=tot % 10
-    cf= ((a % 10) + (b % 10)) // 10
-    cf1=cf // 10
-    tens=tot // 10
+    b=randint(10,a)
+    sub=a-b
+    once=sub % 10
+    if((a % 10) - (b % 10) < 0): 
+        cf=-1
+        cf1=""
+    else:
+        cf=0
+        cf1=" "
+
+
+    tens=sub // 10
 
 
     print("                   " + str(a) )
-    print("                 + " + str(b) )
+    print("                 - " + str(b) )
     print("               ------------")
     #print("enter once digit")
     #c=input()
     #print("                   " + str(a) + "\n""                 + " + str(b)+ "\n               ------------\n" +"                    {0}".format(input("enter once digit:   ")))
     c=input("enter once digit:   ")
-    if str(c) != str(once):
+    if int(c) != int(once):
         print("Sorry, once digit is not correct rignt ans is " + str(once))
         c=input() 
         time.sleep(10)
@@ -37,13 +43,13 @@ for i in range(1, count+1):
     os.system('cls')
     print("enter carry forward")
     print("                    " + str(a) )
-    print("                  + " + str(b) )
+    print("                  - " + str(b) )
     print("                ------------")
     print("                     " + str(c) )
     d=input()
     os.system('cls')
 
-    if str(d) != str(cf):
+    if int(d) != int(cf):
         print("Sorry, carryforward is not correct rignt ans is " + str(cf)+ "\n please enter carryforward again") 
         d=input()
         exit
@@ -51,34 +57,34 @@ for i in range(1, count+1):
         print(" ")
 
 
-    print("                    " + str(d))
+    print("                   "+cf1 + str(d))
     print("                    " + str(a) )
-    print("                  + " + str(b) )
+    print("                  - " + str(b) )
     print("                ------------")
     print("input tens digit     " + str(c) )
     e=input()
     os.system('cls')
 
-    if str(e) != str(tens) :
+    if int(e) != int(tens) :
         print("Sorry, tens is not correct rignt ans is " + str(tens)+ "\n please enter carryforward again") 
         e=input()
         exit
     else :
         print(" ")
 
-    print("                    " + str(d))
+    print("                   "+cf1 + str(d))
     print("                    " + str(a) )
-    print("                  + " + str(b) )
+    print("                  - " + str(b) )
     print("                ------------")
     print("                    "+ str(e) + str(c) )
 
 
     #print(str(ei)+ "!="+ str(tot))
-    if (str(e)+str(c)) != str(tot) :
-        print("Sorry, total is not correct rignt ans is " + str(tot)) 
+    if (int(e)*10+int(c)) != int(sub) :
+        print("Sorry, total is not correct rignt ans is " + str(sub)) 
         exit
     else :
         print("\n\n\n Hurray!!!!!!!!!! its correct")
-    time.sleep(5)
+    time.sleep(8)
     os.system('cls')
     i=i+1
